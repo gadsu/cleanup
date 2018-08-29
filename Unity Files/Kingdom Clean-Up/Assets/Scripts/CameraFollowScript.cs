c13e96a1c3923b5a37daa7c95bdd69bf537315b8
rachelgroth <38136112+rachelgroth@users.noreply.github.com>
2018-08-29 18:31:50 -0500

    basic movement and camera

---
+++ Unity Files/Kingdom Clean-Up/Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour {
+
+    public Transform player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(player.position.x, player.position.y, -10);
+	}
+}
