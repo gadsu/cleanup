45d2c572976bc0763822158f626c6159218f2583
rachelgroth <38136112+rachelgroth@users.noreply.github.com>
2018-08-30 17:59:37 -0500

    better movement
    Co-Authored-By: ladyflyers29 <ladyflyers29@users.noreply.github.com>

--- Unity Files/Kingdom Clean-Up/Assets/Scripts/CharacterMovement.cs
+++ Unity Files/Kingdom Clean-Up/Assets/Scripts/CharacterMovement.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
+﻿/*
+ * Moving the player character and reading inputs
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour {
 
-    public float charSpeed = 5f;
-    public float charForce = 365f;
+    public float charMaxSpeed = 5f;
     public float charJumpSpeed = 10f;
     public bool onGround { get; protected set; }
+    public bool facingRight = true;
 
     Animator an;
     Rigidbody2D rb;
@@ -18,34 +22,44 @@ public class CharacterMovement : MonoBehaviour {
         an = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
         //Horizontal Movement
-        if (Input.GetAxis("Horizontal") == 0) //reset to 0
-        {
-            rb.velocity = new Vector2(0, rb.velocity.y);
-        }
-        else if (Input.GetAxis("Horizontal") > 0 )
-        {
-            rb.velocity = new Vector2(charSpeed, rb.velocity.y);
-        }
-        else if (Input.GetAxis("Horizontal") < 0)
-        {
-            rb.velocity = new Vector2(-1 * charSpeed, rb.velocity.y);
-        }
+        float force = Input.GetAxis("Horizontal");
+        an.SetFloat("Speed", Mathf.Abs(force));
 
-        //Vertical Movement
-        if (Input.GetAxis("Vertical") > 0 && onGround)
+        rb.velocity = new Vector2(force * charMaxSpeed, rb.velocity.y);
+
+        if (force > 0 && !facingRight)
         {
-            rb.velocity = new Vector2(rb.velocity.x, charJumpSpeed);
-            onGround = false;
+            Flip();
+        }
+        else if (force < 0 && facingRight)
+        {
+            Flip();
         }
 
         if (rb.velocity.y == 0)
         {
             onGround = true;
         }
+    }
+
+    private void Update()
+    {
+        //Vertical Movement
+        if ((Input.GetAxis("Vertical") > 0 || Input.GetKeyDown(KeyCode.Space)) && onGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, charJumpSpeed);
+            onGround = false;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y);
     }
 }
---
+++ Unity Files/Kingdom Clean-Up/Assets/Scripts/FloorKillbox.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorKillbox : MonoBehaviour {
+
+    public Transform player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            col.gameObject.transform.position = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            // kill the damn thing
+        }
+    }
+
+
+    
+}
