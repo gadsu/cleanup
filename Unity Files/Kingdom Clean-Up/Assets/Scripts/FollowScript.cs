95c7cba4f003c7d34f1cadcf5dcead812879207a
cjpetersen <25103835+cjpetersen@users.noreply.github.com>
2018-10-03 09:05:48 -0500

    Created a basic AI Nav mesh agent and render

--- Unity Files/Kingdom Clean-Up/Assets/Scripts/FollowScript.cs
+++ Unity Files/Kingdom Clean-Up/Assets/Scripts/FollowScript.cs
@@ -8,47 +8,47 @@ public class FollowScript : MonoBehaviour
 {
 
     NavMeshAgent agent;
-    
-    public Transform target;
+
+    GameObject target = null;
     public Transform[] targetArr;
     private bool playerFound = true;
     public float LostPlayerTimer = 5f;
     public float PatrolTimer = 5f;
     public int PatrolIndex = 0;
-    
+
 
     void Start()
     {
 
+        //GameObject.FindGameObjectsWithTag("player")
+        PlayerFound(target);
     }
 
     void Update()
     {
 
-        
+
 
         if (playerFound == true)
         {
             LostPlayerTimer -= Time.deltaTime;
-            
         }
-        
-        
+
+
         if (LostPlayerTimer <= 0)
         {
             playerFound = false;
-            
         }
-        
+
         if (!playerFound)
         {
             PatrolTimer -= Time.deltaTime;
         }
         else if (target != null)
         {
-            agent.SetDestination(target.position);
+            agent.SetDestination(target.transform.position);
         }
-        
+
 
         if (PatrolTimer <= 5 && playerFound == false && PatrolIndex < targetArr.Length && targetArr[PatrolIndex] != null)
         {
@@ -57,20 +57,19 @@ public class FollowScript : MonoBehaviour
             {
                 PatrolIndex++;
                 PatrolTimer = 5;
-               
+
                 if (PatrolIndex == targetArr.Length)
                 {
                     PatrolIndex = 0;
                 }
             }
         }
-   
+
     }
 
     private void PlayerFound(GameObject player)
     {
         playerFound = true;
-        target = player.transform;
         LostPlayerTimer = 5f;
     }
 
