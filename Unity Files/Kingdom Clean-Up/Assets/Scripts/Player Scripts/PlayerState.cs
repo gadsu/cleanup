91a2283783baacc77f3aaf0dca58932fd4a49df3
ladyflyers29 <42610874+ladyflyers29@users.noreply.github.com>
2018-11-26 10:51:31 -0600

    trying to fix buttons and UI
    Co-Authored-By: Riley Winkler <rex8112@gmail.com>

--- Unity Files/Kingdom Clean-Up/Assets/Scripts/Player Scripts/PlayerState.cs
+++ Unity Files/Kingdom Clean-Up/Assets/Scripts/Player Scripts/PlayerState.cs
@@ -16,12 +16,12 @@ public class PlayerState : MonoBehaviour {
     public int redSlimeMeter;
     [Tooltip("How much blue slime?")]
     public int blueSlimeMeter;
-    [Tooltip("Slider object")]
-    public Slider greenMeter;
-    [Tooltip("Slider object")]
-    public Slider redMeter;
-    [Tooltip("Slider object")]
-    public Slider blueMeter;
+    [Tooltip("Slime Meter object")]
+    public GameObject greenMeter;
+    [Tooltip("Slime Meter object")]
+    public GameObject redMeter;
+    [Tooltip("Slime Meter object")]
+    public GameObject blueMeter;
     [Tooltip("How much health the player has as a float")]
     public float playerHealth = 100f;
     [Tooltip("Can the player be damaged?")]
@@ -31,6 +31,7 @@ public class PlayerState : MonoBehaviour {
     [Tooltip("Frame the player was damaged on")]
     public float damageFrame;
 
+    public GameObject SlimeMeter;
 
     GameObject player;
     int maxSlime = 100;
@@ -48,52 +49,85 @@ public class PlayerState : MonoBehaviour {
         if ((greenSlimeMeter < maxSlime) && type == "green")//Adds green slime to the slime meter
         {
             greenSlimeMeter = Mathf.Clamp(greenSlimeMeter + val, 0, 100);
-            greenMeter.value = greenSlimeMeter;
-            //setSlimeMeterImage(greenSlimeMeter, type);
+            setSlimeMeterImage(greenSlimeMeter, type);
             //Debug.Log("<color=green>GreenSlimeVal:</color> " + greenSlimeMeter);//tells the debug log that green slime was added to the slime meter
         }
 
         if ((redSlimeMeter < maxSlime) && type == "red")//Adds red slime to the slime meter
         {
             redSlimeMeter = Mathf.Clamp(redSlimeMeter + val, 0, 100);
-            redMeter.value = redSlimeMeter;
-            //setSlimeMeterImage(redSlimeMeter, type);
+            setSlimeMeterImage(redSlimeMeter, type);
             //Debug.Log("<color=red>RedSlimeVal:</color> " + redSlimeMeter);//tells the debug log that red slime was added to the slime meter
         }
 
         if ((blueSlimeMeter < maxSlime) && type == "blue")//Adds blue slime to the slime meter
         {
             blueSlimeMeter = Mathf.Clamp(blueSlimeMeter + val, 0, 100);
-            blueMeter.value = blueSlimeMeter;
-            //setSlimeMeterImage(blueSlimeMeter, type);
+            //blueMeter.value = blueSlimeMeter;
+            setSlimeMeterImage(blueSlimeMeter, type);
             //Debug.Log("<color=blue>BlueSlimeVal:</color> " + blueSlimeMeter);//tells the debug log that blue slime was added to the slime meter
         }
 
     }
 
-     //starting logic for switching the image when slime val gets to certain points
+    //starting logic for switching the image when slime val gets to certain points
 
-    //public void setSlimeMeterImage(int val, string type)
-    //{
-    //    GameObject SlimeMeter;
-    //    if ( type == "blue")
-    //    {
-    //        SlimeMeter = blueMeter;
-    //    }
-    //    if (type == "red")
-    //    {
-    //        SlimeMeter = redMeter;
-    //    }
-    //    if (type == "green")
-    //    {
-    //        SlimeMeter = greenMeter;
-    //    }
+    public void setSlimeMeterImage(int val, string type)
+    {
 
-    //    if (val <= 12)
-    //    {
+        if (type == "blue")
+        {
+            SlimeMeter = blueMeter;
+        }
+        if (type == "red")
+        {
+            SlimeMeter = redMeter;
+        }
+        if (type == "green")
+        {
+            SlimeMeter = greenMeter;
+        }
 
-    //    }
-    //}
+        if (val < 25)
+        {
+            SlimeMeter.transform.Find("1").gameObject.SetActive(true);
+        }
+        if (val >= 25 && val < 37)
+        {
+            SlimeMeter.transform.Find("1").gameObject.SetActive(false);
+            SlimeMeter.transform.Find("2").gameObject.SetActive(true);
+        }
+        if (val >= 37 && val < 50)
+        {
+            SlimeMeter.transform.Find("2").gameObject.SetActive(false);
+            SlimeMeter.transform.Find("3").gameObject.SetActive(true);
+        }
+        if (val >= 50 && val < 62)
+        {
+            SlimeMeter.transform.Find("3").gameObject.SetActive(false);
+            SlimeMeter.transform.Find("4").gameObject.SetActive(true);
+        }
+        if (val >= 62 && val < 75)
+        {
+            SlimeMeter.transform.Find("4").gameObject.SetActive(false);
+            SlimeMeter.transform.Find("5").gameObject.SetActive(true);
+        }
+        if (val >= 75 && val < 87)
+        {
+            SlimeMeter.transform.Find("5").gameObject.SetActive(false);
+            SlimeMeter.transform.Find("6").gameObject.SetActive(true);
+        }
+        if (val >= 87 && val < 100)
+        {
+            SlimeMeter.transform.Find("6").gameObject.SetActive(false);
+            SlimeMeter.transform.Find("7").gameObject.SetActive(true);
+        }
+        if (val >= 100)
+        {
+            SlimeMeter.transform.Find("7").gameObject.SetActive(false);
+            SlimeMeter.transform.Find("8").gameObject.SetActive(true);
+        }
+    }
 
     public void takeDamage(float dmg)
     {
@@ -119,9 +153,9 @@ public class PlayerState : MonoBehaviour {
 	
     public void loadScene()
     {
-        greenMeter = GameObject.Find("GreenMeter").GetComponent<Slider>();
-        redMeter = GameObject.Find("RedMeter").GetComponent<Slider>();
-        blueMeter = GameObject.Find("BlueMeter").GetComponent<Slider>();
+        greenMeter = GameObject.Find("greenMeter");
+        redMeter = GameObject.Find("redMeter");
+        blueMeter = GameObject.Find("blueMeter");
 
         player = GameObject.Find("Player");
     }
