e908603a7f7bf06f65ec2abb6ce2664271940850
rachelgroth <38136112+rachelgroth@users.noreply.github.com>
2018-11-28 18:12:59 -0600

    got save/load working
    Co-Authored-By: Carl J. Petersen <cjpetersen@users.noreply.github.com>

--- Unity Files/Kingdom Clean-Up/Assets/Scripts/Menu Scripts/CharCreation.cs
+++ Unity Files/Kingdom Clean-Up/Assets/Scripts/Menu Scripts/CharCreation.cs
@@ -5,18 +5,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharCreation : MonoBehaviour {
 
-    // Use this for initialization
-    void Start() {
+    GameData gd;
 
-    }
-    public void NewSaveFile() 
+    // Use this for initialization
+    void Start()
     {
-        //make a new save file
+        gd = GameObject.Find("DontDestroyOnLoad").GetComponent<GameData>();
+        GameObject.Find("Player Num").GetComponent<Text>().text = "Player " + gd.saveFileNum;
+    }
+    public void saveFile() 
+    {
+        if (GameObject.Find("InputField").GetComponent<InputField>().text.Replace(" ", "").Length > 2)
+            gd.SaveFile();
     }
 	// Update is called once per frame
+    public void changeName()
+    {
+        string name = GameObject.Find("InputField").GetComponent<InputField>().text;
+        gd.gamedic[gd.saveFileNum + "playerName"] = name;
+        Debug.Log("Changed name to " + name);
+    }
 	void Update () {
 		
 	}
---
+++ Unity Files/Kingdom Clean-Up/Assets/Scripts/Menu Scripts/SaveandLoad.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class SaveandLoad : MonoBehaviour {
+    public Text name1;
+    public Text name2;
+    public Text name3;
+    public Text name4;
+    GameData gd;
+
+	// Use this for initialization
+	void Start () {
+        gd = GameObject.Find("DontDestroyOnLoad").GetComponent<GameData>();
+        loadSaveAndLoad();
+    }
+
+    public void loadSaveAndLoad()
+    {
+        name1 = GameObject.Find("Name1").GetComponent<Text>();
+        name1.text = gd.gamedic["1playerName"];
+
+        name2 = GameObject.Find("Name2").GetComponent<Text>();
+        name2.text = gd.gamedic["2playerName"];
+
+        name3 = GameObject.Find("Name3").GetComponent<Text>();
+        name3.text = gd.gamedic["3playerName"];
+
+        name4 = GameObject.Find("Name4").GetComponent<Text>();
+        name4.text = gd.gamedic["4playerName"];
+    }
+    
+    //Called when button is pressed
+    public void SelectFile(string playernum)
+    {
+        //Tell GameData which file to load
+        gd.LoadFile(playernum);
+        //Checking if first time, then moving 
+        if (gd.gamedic[playernum + "newGame"] == "true")
+        {
+            string val = playernum + "newGame";
+            gd.gamedic[val] = "false";
+            //           NEED TO CHANGE STRING TO READ FALSE! (we do)
+            SceneManager.LoadScene("CharacterCustomize");
+        }
+        else if (gd.gamedic[playernum + "newGame"] == "false")
+        {
+            SceneManager.LoadScene("OverWorld");
+        }
+    }
+
+    
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
