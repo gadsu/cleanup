6eb02244cab1495b912e159381384c83814d3dd1
cjpetersen <>
2019-01-23 17:00:06 -0600

    Added CrunkKnight
    Co-Authored-By: Nicholas Lenz <aviazoid@users.noreply.github.com>

---
+++ Unity Files/Kingdom Clean-Up/Assets/Scripts/SoundItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+public class SoundItem
+{
+    //this is the sound item
+    public AudioClip CrunkKnight;
+
+}
