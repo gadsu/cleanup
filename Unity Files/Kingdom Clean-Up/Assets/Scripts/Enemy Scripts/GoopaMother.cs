e70fec5ca8414fc20ff7ea193888d6b863056fef
Riley Winkler <rex8112@gmail.com>
2019-02-13 15:55:48 -0600

    Began work on Goopa Mother
    Idle animation put on the mother and the script was made, only thing on the script is the gooplings list which has already been filled with all of the gooplings
    
    Co-Authored-By: ladyflyers29 <ladyflyers29@users.noreply.github.com>

---
+++ Unity Files/Kingdom Clean-Up/Assets/Scripts/Enemy Scripts/GoopaMother.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoopaMother : MonoBehaviour
+{
+    public List<GameObject> gooplings = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
