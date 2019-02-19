f12691b0ee61f20bb4b50f7cf43eb97d8e158d45
cjpetersen <>
2019-02-19 11:58:45 -0600

    Added to the Goop Mother Ai
    also added the crouch animations to the animations folder but did not implement,

--- Unity Files/Kingdom Clean-Up/Assets/Scripts/Enemy Scripts/GoopaMother.cs
+++ Unity Files/Kingdom Clean-Up/Assets/Scripts/Enemy Scripts/GoopaMother.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class GoopaMother : MonoBehaviour
 {
     public List<GameObject> gooplings = new List<GameObject>();
+    [Tooltip("Setting the prefab for what viscera it spawns")]
+    public GameObject gooplingPrefab;
 
-    // Start is called before the first frame update
-    void Start()
+    public void takeDamage(int dmg)
     {
+
+        GameObject SlimeViscera = Instantiate<GameObject>(gooplingPrefab);//, currentPos.position, currentPos.rotation);
+        SlimeViscera.transform.localScale = new Vector3(2.5f, 2.5f, 0);
         
     }
+    
+       
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    
+
 }
