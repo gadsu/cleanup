889edc2c8d108afd14588ff12397ca7d86ea7b60
Riley Winkler <rex8112@gmail.com>
2019-04-24 15:21:39 -0500

    adds boss camera transitions
    Co-Authored-By: Carl J. Petersen <cjpetersen@users.noreply.github.com>
    Co-Authored-By: ladyflyers29 <ladyflyers29@users.noreply.github.com>

---
+++ Unity Files/Kingdom Clean-Up/Assets/Scripts/barnCameraTransition.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barnCameraTransition : MonoBehaviour
+{
+    public GameObject vcam;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == player.name)
+        {
+            vcam.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == player.name)
+        {
+            vcam.SetActive(true);
+        }
+    }
+}
